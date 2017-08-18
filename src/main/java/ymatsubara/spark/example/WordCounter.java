5a2d5200116d899e55ccab8c7d68c5d16644a402
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2017-08-18 22:30:00 +0900

    refactored

--- src/main/java/ymatsubara/spark/example/WordCounter.java
+++ src/main/java/ymatsubara/spark/example/WordCounter.java
@@ -9,17 +9,26 @@ import scala.Tuple2;
 import java.util.Arrays;
 
 public class WordCounter {
-    public static void main(String[] args) {
-        String master = "local[*]";
-        SparkConf conf = new SparkConf()
-                .setAppName("WordCounter")
-                .setMaster(master);
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> textFile = sc.textFile("src/main/resources/input/wordcount/article*.txt");
+    public static final String APP_NAME = "WordCounter";
+    public static final String MASTER = "local[*]";
+    public static final String INPUT_FILE_PATH = "src/main/resources/input/wordcount/article*.txt";
+    public static final String OUTPUT_DIR_PATH = "src/main/resources/output/wordcount/";
+
+    public static JavaPairRDD<String, Integer> count(JavaRDD<String> textFile) {
         JavaPairRDD<String, Integer> wordCountPair = textFile
                 .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
                 .mapToPair(word -> new Tuple2<>(word.replace(".", "").toLowerCase(), 1))
                 .reduceByKey((a, b) -> a + b);
-        wordCountPair.saveAsTextFile("src/main/resources/output/wordcount/");
+        return wordCountPair;
+    }
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf()
+                .setAppName(APP_NAME)
+                .setMaster(MASTER);
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> textFile = sc.textFile(INPUT_FILE_PATH);
+        JavaPairRDD<String, Integer> wordCountPair = count(textFile);
+        wordCountPair.saveAsTextFile(OUTPUT_DIR_PATH);
     }
 }
